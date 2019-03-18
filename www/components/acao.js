2fc3f0e3e931b7555769bb7317c7195d24a3f260
LuizGustavoMFerreira <luizmisslin@hotmail.com>
2019-03-19 04:04:34 +0900

    Falta terminar o css, termina ai erick

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,51 +1,33 @@
 var quantidade;
 var resertar;
-var sexo;
-
-$(document).on("click", "#radio", function () {
-
-  var sexo = $("#radio").val();
-  var masculino = $("#masculino").val(masculino);
-  var feminino = $("#feminino").val(feminino);
-
-});
+var radio;
 
 $(document).on("click", "#calcular", function () {
 
   var peso = $("#peso").val();
   var altura = $("#altura").val();
   var idade = $("#idade").val();
+  var masculino = $("#masculino").val();
+  var feminino = $("#feminino").val();
+  var nivelAtividade = $("#nivelAtividade").val();
   var imb = $("#imb").val();
-
-   var masculino = (66 + (13.7 * peso + (5 * altura - (6.8 * idade))));
-   var feminino =  (655 + (9.6 * peso + (1.8 * altura - (4.7 * idade))));
-  
-  if (imb < 1.2) {     
-     quantidade = "Pouco ou nenhum exercício";
-  }
-  else if ((imb >= 1.2) && (imb <= 1.375)) {
-     quantidade = "Exercício leve 1 a 3 dias por semana";
-  }
-  else if ((imb >= 1.55) && (imb <= 1.725)) {
-    quantidade = "Exercício moderado, faz esportes 3 a 5 dias por semana";
-  }
-  else if ((imb >= 1.725) && (imb <= 1.9)){
-     quantidade = "Exercício pesado de 5 a 6 dias por semana";
-  }
-  else if (imb > 1.9) {
-     quantidade = "Exercício pesado diariamente e até 2 vezes por dia";
-  }
-
-
-  $("#quantidade").val(quantidade);
   $("#imb").val(imb);
-  }); 
+});
+  
+  if (radio == masculino)
+  {
+    imb = nivelAtividade * (66 + (13,7 * Peso +  (5 * altura - (6,8 * idade))));
+  }
+  else if (radio == feminino)
+  {
+    imb = nivelAtividade * (655 + (13,7 * Peso + ( 5 * altura - (6,8 * idade))));
+  }
+
 
  $(document).on("click", "#resertar", function () {
 
   var peso = $("#peso").val("");
   var altura = $("#altura").val("");
   var idade = $("#idade").val("");
-  var imb = $("#imb").val("")
-  var quantidade = $("#quantidade").val("");
-});     
+  var imb = $("#imb").val("");
+});  
